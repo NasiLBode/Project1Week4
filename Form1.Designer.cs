ded007b9c33e27d08ea1e1e9231b16c4d3a88443
Nasi Bode <nasibode@gmail.com>
2021-09-21 02:54:04 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace Project1Week4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.mortgagePay = new System.Windows.Forms.Label();
+            this.outStandingAmt = new System.Windows.Forms.Label();
+            this.mortgagePayInput = new System.Windows.Forms.TextBox();
+            this.outStandPayInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(158, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 85);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate";
+            this.button1.UseMnemonic = false;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // mortgagePay
+            // 
+            this.mortgagePay.AutoSize = true;
+            this.mortgagePay.Location = new System.Drawing.Point(12, 65);
+            this.mortgagePay.Name = "mortgagePay";
+            this.mortgagePay.Size = new System.Drawing.Size(223, 19);
+            this.mortgagePay.TabIndex = 1;
+            this.mortgagePay.Text = "Monthly Mortgage Payment:";
+            this.mortgagePay.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // outStandingAmt
+            // 
+            this.outStandingAmt.AutoSize = true;
+            this.outStandingAmt.Location = new System.Drawing.Point(60, 123);
+            this.outStandingAmt.Name = "outStandingAmt";
+            this.outStandingAmt.Size = new System.Drawing.Size(175, 19);
+            this.outStandingAmt.TabIndex = 2;
+            this.outStandingAmt.Text = "Outstanding Balance:";
+            this.outStandingAmt.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // mortgagePayInput
+            // 
+            this.mortgagePayInput.Location = new System.Drawing.Point(251, 58);
+            this.mortgagePayInput.Name = "mortgagePayInput";
+            this.mortgagePayInput.Size = new System.Drawing.Size(199, 26);
+            this.mortgagePayInput.TabIndex = 3;
+            // 
+            // outStandPayInput
+            // 
+            this.outStandPayInput.Location = new System.Drawing.Point(251, 116);
+            this.outStandPayInput.Name = "outStandPayInput";
+            this.outStandPayInput.Size = new System.Drawing.Size(199, 26);
+            this.outStandPayInput.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(522, 318);
+            this.Controls.Add(this.outStandPayInput);
+            this.Controls.Add(this.mortgagePayInput);
+            this.Controls.Add(this.outStandingAmt);
+            this.Controls.Add(this.mortgagePay);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Mortgage Payments";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label mortgagePay;
+        private System.Windows.Forms.Label outStandingAmt;
+        private System.Windows.Forms.TextBox mortgagePayInput;
+        private System.Windows.Forms.TextBox outStandPayInput;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿/*File Name: Project1Week4
+ * Programmer: Nasi Bode
+ * Problem Statement: Write a GUI program that reads in the amount of a monthly mortgage payment
+ * and the outstanding balance (i.e., the amount still owed) and then outputs the amount of
+ * the payment that goes to interest and the amount that goes to principal (i.e., the amount
+ * that goes to reducing the debt). Assume that the annual interest payment is 6.39 percent. 
+ * Use a defined constant for the interest rate. Note that payments are made monthly,
+ * so the interest is only one-twelfth of the annual interest rate of 6.39 percent.
+ * Overall Plan: 
+ * 1. Create GUI Application
+ * 2. Create textboxes for user input (monthly payment and outstanding balance)
+ * 3. Create labels for user 
+ * 4. Create button to calculate the results
+ * 5. get input from users and convert the string values to doubles
+ * 6. figure out how many years it will take to pay off mortgage
+ * 7. Solve for principle using a mortgage formula (monthly payment, interest rate, and time are found)
+ * 8. Calculate monthly interest rate
+ * 9. Subtract monthly interest rate from monthly pay to determine the amount of the mortgage payment that goes to reducing principle
+ * 10. Display results to the user
+ */
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project1Week4
+{
+    public partial class Form1 : Form
+    {
+        public const double interestRate = 6.39 / 100;
+        
+            public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //get double values from user input
+            double monthlyPay = double.Parse(mortgagePayInput.Text);
+            double outStandBalance = double.Parse(outStandPayInput.Text);
+
+            //create amount of years it will take to pay off debt
+            double n = (outStandBalance / monthlyPay);
+            //original formula: monthly payment = principle * (interestRate * (1 + interestRate)^time /
+            //divided by:
+            // (1 + interestRate)^ time - 1
+  
+
+            //create variable to perform the exponent method
+            double var1 = (1 + interestRate);
+            //get value for exponent
+            double variable1 = Math.Pow(var1, n);
+            ;
+            //solve for principle
+
+            double principle = (monthlyPay * ((variable1) - 1)) / (interestRate * variable1);
+            //calculate monthly interest rate
+            double monthlyIntAmt= (principle * interestRate) / 12;
+            //calculate what part of the payment reduces the principle amount
+            double reducePrincipal = monthlyPay - monthlyIntAmt;
+
+            //display results to screen
+            MessageBox.Show($"Your Principle is ${Math.Round(principle, 2)}\nThe Monthly Interest Rate is: ${Math.Round(monthlyIntAmt, 2)}\nThe Amount that goes to Principle: ${Math.Round(reducePrincipal, 2)}");
+
+
+        }
+    }
+}
